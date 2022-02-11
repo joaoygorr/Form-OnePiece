9075247e3575027de0120fbe90b985a4048fa7bc
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2022-02-11 10:47:48 -0400

    .

--- src/db.py
+++ src/db.py
@@ -1,24 +1,33 @@
-# Importadno pymysql
+# Importando pyodbc
+import pyodbc
 
-
+# Conectando e verificando conexão
 try: 
-  con = pymysql.connect(host="localhost", user="root", password="root")
-  cursor = con.cursor()
+  con = pyodbc.connect("DRIVER={MySQL ODBC 8.0 ANSI Driver}; SERVER=localhost; UID=root; PWD=root") 
   print("Database Successfully Connected...")
 except Exception as e: 
   print("Connection Error: ", e)
 
-lista = ['Ilha dos Homens-Peixe','Retorno a Sabaody', '517', "Não", '26/04/2002', 'teste']
-  
-# Tabelas 
+# Criando Banco e Tabela  
 with con: 
-  cursor = con.cursor()
-  cursor.execute("CREATE DATABASE if not exists consulta;")
-  cursor.execute("use consulta;")
+  cursor = con.cursor()  
+  cursor.execute("CREATE DATABASE IF NOT EXISTS consulta;")
+  cursor.execute("USE consulta;")
   cursor.execute("CREATE TABLE if not exists Piece(id INTEGER PRIMARY KEY AUTO_INCREMENT, Saga VARCHAR(50), Arcos VARCHAR(50), Ep CHAR(4), filler char(3), Data DATE, Descricao VARCHAR(150) DEFAULT '');")
-  
+
+# Exibindo dados 
 with con:
     cursor = con.cursor()
-    query = "INSERT INTO Piece (Saga, Arcos, Ep, Filler, Data, Descricao) VALUES (%s, %s, %s, %s, %s, %s)"  
-    dados = (str(lista))
-    cursor.execute(query, dados)
+    query = "SELECT * FROM Piece"  
+    cursor.execute(query)
+    info = cursor.fetchall()
+    print(info)
+
+
+
+  
+# with con:
+#     cursor = con.cursor()
+#     query = "INSERT INTO Piece (Saga, Arcos, Ep, Filler, Data, Descricao) VALUES (%s, %s, %s, %s, %s, %s)"  
+#     dados = (str(lista))
+#     cursor.execute(query, dados)
